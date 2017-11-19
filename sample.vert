c1dca36116e2dde3bb023b0c59678d430f435391
Marc-antoine GALLO <mgallo@e1r9p13.42.fr>
2017-11-19 22:50:22 +0100

    test shader

--- sample.vert
+++ sample.vert
@@ -1,14 +1,7 @@
 #version 330 core
 layout(location = 0) in vec3 vertexPosition;
-layout(location = 1) in vec3 vertexColor;
-layout(location = 2) in vec2 vertexTexture;
-
-uniform mat4 projection;
-uniform mat4 model;
-uniform mat4 view;
 
 void main()
 {
-	mat4 mvp = projection * view * model;
-	gl_Position = mvp * vec4(vertexPosition, 1);
+	gl_Position = vec4(vertexPosition, 1);
 }
--- sources/humangl.cpp
+++ sources/humangl.cpp
@@ -16,15 +16,43 @@ int main()
 		else
 		{
 			Shaders sample;
-			if (!sample.loadVertexShader("sample.vert"))
-				return (0);
 			if (!sample.loadFragmentShader("sample.frag"))
+				return (0);
+			if (!sample.loadVertexShader("sample.vert"))
 				return (0);
 			if (!sample.build())
 				return (0);
+
+
+			GLuint VertexArrayID;
+			glGenVertexArrays(1, &VertexArrayID);
+			glBindVertexArray(VertexArrayID);
+			static const GLfloat g_vertex_buffer_data[] = {
+			   -1.0f, -1.0f, 0.0f,
+			   1.0f, -1.0f, 0.0f,
+			   0.0f,  1.0f, 0.0f,
+			};
+			GLuint vertexbuffer;
+			glGenBuffers(1, &vertexbuffer);
+			glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+			glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+			glEnableVertexAttribArray(0);
+			glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+			glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+			glBindBuffer(GL_ARRAY_BUFFER, 0);
+			glBindVertexArray(0);
+
+
 			while (win.isOpen())
 			{
 				win.makeContextCurrent();
+
+				
+				sample.bind();
+				glBindVertexArray(VertexArrayID);
+				glDrawArrays(GL_TRIANGLES, 0, 3);
+				glBindVertexArray(0);
+
 
 				win.update();
 			}
