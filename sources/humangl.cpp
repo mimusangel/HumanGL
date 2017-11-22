81690fd2e01f0147ffdfc76dbe8443db506d3141
Marc-antoine GALLO <mgallo@e1r13p11.42.fr>
2017-11-22 01:20:24 +0100

    clear

--- sources/humangl.cpp
+++ sources/humangl.cpp
@@ -32,32 +32,10 @@ int main()
 				return (0);
 			Cube::Load();
 			Human human;
-			// Cube body(Vec3(0, 0.375f, 0),	Vec3(),	Vec3(0.0f, 0.5f, 0.0f), Vec3(0.25f, 0.375f, 0.125f));
-			// Cube head(Vec3(0, 1, 0),	Vec3(), 	Vec3(0.0f, 0.5f, 0.0f), Vec3(0.25f));
-			// Cube leftArmUp(Vec3(-0.5, 1, 0),	Vec3(),	Vec3(-0.5, -0.5, 0.f), Vec3(0.1249f, 0.1875f, 0.1249f));
-			// Cube leftArmDown(Vec3(-0.5, -1, 0),	Vec3(),	Vec3(0, -0.5, 0), Vec3(0.1249f, 0.1875f, 0.1249f));
-			// Cube leftLegUp(Vec3(-0.25, 0, 0),	Vec3(),	Vec3(0, -0.5, 0.f), Vec3(0.1249f, 0.1875f, 0.1249f));
-			// Cube leftLegDown(Vec3(0, -1, 0),	Vec3(),	Vec3(0, -0.5, 0), Vec3(0.1249f, 0.1875f, 0.1249f));
-			// Cube rightArmUp(Vec3(0.5, 1, 0),	Vec3(),	Vec3(0.5, -0.5, 0.f), Vec3(0.1249f, 0.1875f, 0.1249f));
-			// Cube rightArmDown(Vec3(0.5, -1, 0),	Vec3(),	Vec3(0, -0.5, 0), Vec3(0.1249f, 0.1875f, 0.1249f));
-			// Cube rightLegUp(Vec3(0.25, 0, 0),	Vec3(),	Vec3(0, -0.5, 0.f), Vec3(0.1249f, 0.1875f, 0.1249f));
-			// Cube rightLegDown(Vec3(0, -1, 0),	Vec3(),	Vec3(0, -0.5, 0), Vec3(0.1249f, 0.1875f, 0.1249f));
-			// head.setParent(&body);
-			// leftArmUp.setParent(&body);
-			// leftArmDown.setParent(&leftArmUp);
-			// leftLegUp.setParent(&body);
-			// leftLegDown.setParent(&leftLegUp);
-			// rightArmUp.setParent(&body);
-			// rightArmDown.setParent(&rightArmUp);
-			// rightLegUp.setParent(&body);
-			// rightLegDown.setParent(&rightLegUp);
-			// Vec3	rot;
-			// Vec3	rot2;
 
 			glEnable(GL_DEPTH_TEST);
 			glEnable(GL_CULL_FACE);
 			glCullFace(GL_FRONT);
-			// Debug::print(model);
 			Camera	cam(win);
 
 			cam.translate(Vec3(0, 0, -2));
@@ -68,18 +46,6 @@ int main()
 				/* ******************** */
 				/* * UPDATE           * */
 				/* ******************** */
-				// rot[1] += 0.01f;
-				// rot2[2] += 0.01f;
-				// rot2[2] = TORADIANS(90);
-				// head.setRotate(rot);
-				// leftArmUp.setRotate(rot2.negate());
-				// leftArmDown.setRotate(rot2.negate());
-				// leftLegUp.setRotate(rot2.negate());
-				// leftLegDown.setRotate(rot2.negate());
-				// rightArmUp.setRotate(rot2);
-				// rightArmDown.setRotate(rot2);
-				// rightLegUp.setRotate(rot2);
-				// rightLegDown.setRotate(rot2);
 				cam.move();
 				if (glfwGetMouseButton(win.getGLFW(), GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS ||
 					win.isGrabbed())
@@ -90,12 +56,8 @@ int main()
 						euler += win.dirMouseSmooth;
 						win.dirMouseSmooth = Vec2(0);
 						cam.setRotate(Vec3(TORADIANS(euler), 0));
-						// cam.rotate(up, TORADIANS(win.dirMouse[0]));
-						// cam.rotate(right, TORADIANS(win.dirMouse[1]));
 					}
-					// Debug::print(win.dirMouse);
 				}
-				// cam_move(win, cam);
 				human.update(glfwGetTime());
 				/* ******************** */
 				/* * RENDU            * */
@@ -106,54 +68,9 @@ int main()
 				sample.bind();
 				sample.uniformMat4((GLchar *)"projection", (GLfloat *)&win.matProjection);
 				Mat4x4 viewMat = cam.toMatrix();
-				// Debug::print(viewMat);
 				sample.uniformMat4((GLchar *)"view", (GLfloat *)&viewMat);
 				human.render(sample);
-				// Mat4x4 model = body.toMatrix();
-				// sample.uniformMat4((GLchar *)"model", (GLfloat *)&model);
-				// Cube::Render();
-				// model = head.toMatrix();
-				// sample.uniformMat4((GLchar *)"model", (GLfloat *)&model);
-				// Cube::Render();
-				// model = leftArmUp.toMatrix();
-				// sample.uniformMat4((GLchar *)"model", (GLfloat *)&model);
-				// Cube::Render();
-				// model = leftArmDown.toMatrix();
-				// sample.uniformMat4((GLchar *)"model", (GLfloat *)&model);
-				// Cube::Render();
-				// model = leftLegUp.toMatrix();
-				// sample.uniformMat4((GLchar *)"model", (GLfloat *)&model);
-				// Cube::Render();
-				// model = leftLegDown.toMatrix();
-				// sample.uniformMat4((GLchar *)"model", (GLfloat *)&model);
-				// Cube::Render();
-				// model = rightArmUp.toMatrix();
-				// sample.uniformMat4((GLchar *)"model", (GLfloat *)&model);
-				// Cube::Render();
-				// model = rightArmDown.toMatrix();
-				// sample.uniformMat4((GLchar *)"model", (GLfloat *)&model);
-				// Cube::Render();
-				// model = rightLegUp.toMatrix();
-				// sample.uniformMat4((GLchar *)"model", (GLfloat *)&model);
-				// Cube::Render();
-				// model = rightLegDown.toMatrix();
-				// sample.uniformMat4((GLchar *)"model", (GLfloat *)&model);
-				// Cube::Render();
-
-				// sample.uniformMat4((GLchar *)"model", (GLfloat *)&model_body);
-				// Cube::Render();
-				// sample.uniformMat4((GLchar *)"model", (GLfloat *)&model_head);
-				// Cube::Render();
-				// sample.uniformMat4((GLchar *)"model", (GLfloat *)&model_right_leg);
-				// Cube::Render();
-				// sample.uniformMat4((GLchar *)"model", (GLfloat *)&model_left_leg);
-				// Cube::Render();
-				// sample.uniformMat4((GLchar *)"model", (GLfloat *)&model_right_arm);
-				// Cube::Render();
-				// sample.uniformMat4((GLchar *)"model", (GLfloat *)&model_left_arm);
-				// Cube::Render();
 				win.update();
-			} // GL_LINE_STRIP
 		}
 	}
 	Cube::Unload();
