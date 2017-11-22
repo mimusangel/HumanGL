d5bb880bf085ca62a0ddac220d6ca522f5835832
Jessy ROUTHIER <jrouthie@e1r12p14.42.fr>
2017-11-22 22:50:16 +0100

    cam update

--- include/camera.hpp
+++ include/camera.hpp
@@ -10,11 +10,14 @@ class	Camera : public Transform
 {
 public:
 	Camera(Window &win, const Vec3 &pos = Vec3(), const Quat &rot = Quat(0, 0, 0, 1));
-	void		move(void);
+	Camera		&move(void);
+	Camera		&setRotateEuler(const Vec3 &euler);
+	Camera		&rotateEuler(const Vec3 &euler);
 	Camera		&translate(const Vec3 &value);
 
 private:
 	Window	*_win;
+	Vec3	_euler;
 };
 
 #endif
