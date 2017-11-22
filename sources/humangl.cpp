b6039c115a6264fe62ec9849cb007daff585a69d
Jessy ROUTHIER <jrouthie@e1r12p14.42.fr>
2017-11-22 23:08:58 +0100

    rev

--- sources/humangl.cpp
+++ sources/humangl.cpp
@@ -39,7 +39,7 @@ int main()
 			Cube::Load();
 			Human human;
 			Camera	cam(win);
-			// Anim	anim(10);
+
 			Anim	animList[8] = {
 				AnimManager::loadIdle(),
 				AnimManager::loadWalk(0.2),
@@ -50,11 +50,6 @@ int main()
 				AnimManager::loadNeo(),
 				AnimManager::loadCloClo()
 			};
-			// Anim	anim = AnimManager::loadWalk(0.2);
-			// Anim	anim = AnimManager::loadNeo();
-			// anim.setLoop(true);
-			// human.setAnim(&anim);
-			human.setAnim(&animList[0]);
 			cam.translate(Vec3(2, 1.5, 2));
 			cam.setRotateEuler(Vec3(TORADIANS(-135), TORADIANS(30), 0));
 			while (win.isOpen())
@@ -78,9 +73,6 @@ int main()
 					human.setAnim(animList + 6);
 				if (glfwGetKey(win.getGLFW(), GLFW_KEY_8))
 					human.setAnim(animList + 7);
-
-				// if (glfwGetMouseButton(win.getGLFW(), GLFW_MOUSE_BUTTON_MIDDLE) == GLFW_PRESS)
-				// 	human.setAnim(&anim);
 
 				cam.move();
 				if (glfwGetMouseButton(win.getGLFW(), GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS ||
